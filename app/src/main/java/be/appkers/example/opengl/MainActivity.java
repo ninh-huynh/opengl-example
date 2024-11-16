5a0fa99a8624aff78ebc969282a758fc61ed05bf
Ninh Huynh <kimninh1610@gmail.com>
2024-11-16 13:02:42 +0700

    update targetSdkVersion 35

--- app/src/main/java/be/appkers/example/opengl/MainActivity.java
+++ app/src/main/java/be/appkers/example/opengl/MainActivity.java
@@ -6,7 +6,7 @@ import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLUtils;
 import android.opengl.Matrix;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
